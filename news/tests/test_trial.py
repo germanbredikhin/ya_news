a06871e6367ab6730e57721c721057214fa33e23
german_bredikhin <germanbredikhin@yandex.ru>
2024-05-22 13:12:42 +0800

    routes tests added

---
+++ news/tests/test_trial.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+
+from news.models import News
+
+User = get_user_model()
+
+
+class TestNews(TestCase):
+
+    TITLE = 'News title'
+    TEXT = 'Test text'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title=cls.TITLE,
+            text=cls.TEXT
+        )
+
+    def test_successful_creation(self):
+        news_count = News.objects.count()
+        self.assertEqual(news_count, 1)
+
+    def test_title(self):
+        self.assertEqual(self.news.title, self.TITLE)
+
+
+class TestNews2(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='testUser')
+        cls.user_client = Client()
+        cls.user_client.force_login(cls.user)
